2e9547646c908f8533df8d4811a90b26599b617e
Sagar Chaudhary <sagar.c@productivet.com>
2019-10-05 19:01:44 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,27 @@ export class AppComponent {
   value = 0;
   showErrMsg;
   showErrMsgValid;
+  denominations = [2000, 500, 200, 100, 50, 20, 10];
   // #077696
 
   addItem() {
     this.showErrMsg = '';
     if (this.checkOverFlow()) {
       return;
-    } else {
-    this.value += this.item['denomination'] * this.item['total'];
-    this.allItems.push(this.item);
-    this.item = {};
     }
+    this.value += this.item['denomination'] * this.item['total'];
+    if (this.matchFound(this.item['denomination']) > -1) {
+      this.allItems[this.matchFound(this.item['denomination'])].total += this.item['total'];
+    } else {
+      this.allItems.push(this.item);
+    }
+    this.item = {};
+  }
+
+  matchFound(denomination) {
+    return this.allItems.findIndex((item) => {
+      return item.denomination == denomination;
+    });
   }
 
   checkOverFlow() {
@@ -51,15 +61,22 @@ export class AppComponent {
     this.allItems = [];
     this.value = 0;
     let amtToBreak = this.amount;
-    this.prepareItems(amtToBreak, 2000);
-    amtToBreak = amtToBreak % 2000;
-    if (amtToBreak > 0) {
-      this.prepareItems(amtToBreak, 500);
-    }
-    amtToBreak = amtToBreak % 500;
-    if (amtToBreak > 0) {
-      this.prepareItems(amtToBreak, 200);
-    }
+    this.denominations.forEach(denomination => {
+      if (amtToBreak > 0) {
+        this.prepareItems(amtToBreak, denomination);
+        amtToBreak = amtToBreak % denomination;
+      }
+    });
+  }
+
+  breakFromHere() {
+    let amtToBreak = this.amount - this.value;
+    this.denominations.forEach(denomination => {
+      if (amtToBreak > 0 && this.matchFound(denomination) === -1) {
+        this.prepareItems(amtToBreak, denomination);
+        amtToBreak = amtToBreak % denomination;
+      }
+    });
   }
 
   prepareItems(amtToBreak, denomination) {
